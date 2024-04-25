b67aa4a21bc7b7f5f046ccf64c59de0d0bec783b
anyi-sunny <asunnysky65@gmail.com>
2024-04-25 06:52:52 -0400

    Fixed unlog and changed logic for parking spot verifications

--- java-code.js
+++ java-code.js
@@ -1,28 +1,72 @@
-const occupied = JSON.parse(localStorage.getItem('occupied')) || [];
+const farley = JSON.parse(localStorage.getItem('farley')) || [];
+const coffin = JSON.parse(localStorage.getItem('coffin')) || [];
+const admissions = JSON.parse(localStorage.getItem('admissions')) || [];
 
-function spot(spot) {
-    if (occupied.includes(spot)) {
-        localStorage.setItem("Spot", "Spot is occupied")
+function spot(spot, lot) {
+  if (lot === 'flot') {
+    if (farley.includes(spot)) {
+      localStorage.setItem("Spot", "Spot is occupied")
     }
     else {
-        occupied.push(spot)
-        if (spot%6 === 0) {
-          localStorage.setItem("Spot", 6)
-        }
-        else {
-          localStorage.setItem("Spot", spot%6)
-        }
+      farley.push(spot)
+      localStorage.setItem("Spot", spot)
     }
-    localStorage.setItem('occupied', JSON.stringify(occupied));
+    localStorage.setItem('farley', JSON.stringify(farley))
+  }
+  if (lot === 'clot') {
+    if (coffin.includes(spot)) {
+      localStorage.setItem("Spot", "Spot is occupied")
+    }
+    else {
+      coffin.push(spot)
+      localStorage.setItem("Spot", spot)
+    }
+    localStorage.setItem('coffin', JSON.stringify(coffin))
+  }
+  if (lot === 'alot') {
+    if (admissions.includes(spot)) {
+      localStorage.setItem("Spot", "Spot is occupied")
+    }
+    else {
+      admissions.push(spot)
+      localStorage.setItem("Spot", spot)
+    }
+    localStorage.setItem('admissions', JSON.stringify(admissions))
+  }
 }
 
-function unlog(spot) {
-    if (!occupied.includes(spot)) {
+function unlog(spot, lot) {
+  if (lot === 'flot') {
+    if (!farley.includes(spot)) {
       localStorage.setItem("Spot", "Spot is already empty")
     }
     else {
-      const index = occupied.indexOf(spot);
-      occupied.splice(index, 1)
-      localStorage.setItem('occupied', JSON.stringify(occupied))
+      const index = farley.indexOf(spot);
+      farley.splice(index, 1)
+      localStorage.setItem('farley', JSON.stringify(farley))
     }
+  }
+
+  if (lot === 'clot') {
+    if (!coffin.includes(spot)) {
+      localStorage.setItem("Spot", "Spot is already empty")
+    }
+    else {
+      const index = coffin.indexOf(spot);
+      coffin.splice(index, 1)
+      localStorage.setItem('coffin', JSON.stringify(coffin))
+    }
+  }
+
+  if (lot === 'alot') {
+    if (!admissions.includes(spot)) {
+      localStorage.setItem("Spot", "Spot is already empty")
+    }
+    else {
+      const index = admissions.indexOf(spot);
+      admissions.splice(index, 1)
+      localStorage.setItem('admissions', JSON.stringify(admissions))
+    }
+  }
+
 }
