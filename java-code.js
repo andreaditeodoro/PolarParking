abe571be5a647f24a016cf2931cdfcc3aa4a127b
anyi-sunny <asunnysky65@gmail.com>
2024-05-13 19:13:26 -0400

    Update java-code.js

--- java-code.js
+++ java-code.js
@@ -2,6 +2,8 @@ const farley = JSON.parse(localStorage.getItem('farley')) || [];
 const coffin = JSON.parse(localStorage.getItem('coffin')) || [];
 const admissions = JSON.parse(localStorage.getItem('admissions')) || [];
 
+//log a spot
+//updates a list of occoupied spots and our spot item
 function spot(spot, lot) {
   if (lot === 'flot') {
     if (farley.includes(spot)) {
@@ -9,7 +11,7 @@ function spot(spot, lot) {
     }
     else {
       farley.push(spot)
-      localStorage.setItem("Spot", "You have selected spot: " + spot.toString())
+      localStorage.setItem("Spot", "You have successfully logged spot: " + spot.toString())
     }
     localStorage.setItem('farley', JSON.stringify(farley))
   }
@@ -19,7 +21,7 @@ function spot(spot, lot) {
     }
     else {
       coffin.push(spot)
-      localStorage.setItem("Spot", "You have selected spot: " + spot.toString())
+      localStorage.setItem("Spot", "You have successfully logged spot: " + spot.toString())
     }
     localStorage.setItem('coffin', JSON.stringify(coffin))
   }
@@ -29,12 +31,14 @@ function spot(spot, lot) {
     }
     else {
       admissions.push(spot)
-      localStorage.setItem("Spot", "You have selected spot: " + spot.toString())
+      localStorage.setItem("Spot", "You have successfully logged spot: " + spot.toString())
     }
     localStorage.setItem('admissions', JSON.stringify(admissions))
   }
 }
 
+//unlog function
+//takes in the spot number and which lot we are in and updates the spot item and list of occupied spots
 function unlog(spot, lot) {
   if (lot === 'flot') {
     if (!farley.includes(spot)) {
@@ -73,6 +77,7 @@ function unlog(spot, lot) {
 }
 }
 
+//set the setting
 function setSetting(option) {
   document.getElementById("profile").onchange = function() {
     let newSelected = document.getElementById("profile").value;
